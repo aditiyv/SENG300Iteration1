4828e922ddba9cd9640984d2dee847a2e84928ec
BrettLyle11 <lylebrett11@gmail.com>
2023-03-13 13:00:05 -0600

    Updated class more so correctly but still some issues

--- com.autovend.software/src/com/autovend/software/Cart.java
+++ com.autovend.software/src/com/autovend/software/Cart.java
@@ -1,24 +1,33 @@
 package com.autovend.software;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 
-import com.autovend.SellableUnit;
 import com.autovend.devices.BarcodeScanner;
 import com.autovend.devices.DisabledException;
+import com.autovend.devices.ElectronicScale;
 import com.autovend.devices.SimulationException;
 import com.autovend.products.BarcodedProduct;
 import com.autovend.products.Product;
+import com.autovend.*;
 
 public class Cart {
 	public Cart () {
+		es = new ElectronicScale(5000,50);
 	}
 	//will store all the items in one transaction
-	ArrayList<SellableUnit> cartOfItems;
+	ArrayList<Product> cartOfItems;
+	//Total price 
+	public BigDecimal total;
+	private ElectronicScale es;
 	//adds item by scan
-	public boolean AddByScan (SellableUnit bp, BarcodeScanner bs) {
+	public boolean AddByScan (BarcodedProduct bp, BarcodeScanner bs) {
+		BarcodedUnit unit = new BarcodedUnit(bp.getBarcode(),bp.getExpectedWeight());
 		try {
-		if(bs.scan(bp)){
+		if(bs.scan(unit)){
+			es.add(unit);
 			cartOfItems.add(bp);
+			total = total.add(bp.getPrice());
 			return true;
 		}
 		}
@@ -31,8 +40,11 @@ public class Cart {
 		return false;
 	}
 	//returns the cart
-	public ArrayList<SellableUnit> getCart() {
+	public ArrayList<Product> getCart() {
 		return cartOfItems;
+	}
+	public BigDecimal getPrice() {
+		return total;
 	}
 	
 }
