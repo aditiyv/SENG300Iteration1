425cd5748342e11c752064d0a159cf9a0285a4f9
Nam Nguyen Vu <namnguyen.vu1@ucalgary.ca>
2023-03-14 10:36:01 -0600

    ok

--- com.autovend.software/src/com/autovend/software/PayByCredit.java
+++ com.autovend.software/src/com/autovend/software/PayByCredit.java
@@ -1,5 +1,15 @@
 package com.autovend.software;
 
-public class PayByCredit {
+import java.util.Scanner;
+
+public class PayByCredit extends Pay {
+
+	@Override
+	public void pay() {
+		// TODO Auto-generated method stub
+		System.out.println("Please insert card");
+		Scanner scanner = new Scanner(System.in);
+		int cardNumber = scanner.nextInt();
+	}
 
 }
