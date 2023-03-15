00528dffc0f1f8f107dfcd3a99034e74cb82b29a
Nam Nguyen Vu <namnguyen.vu1@ucalgary.ca>
2023-03-15 10:49:15 -0600

    some fixes

--- com.autovend.software/src/com/autovend/software/Pay.java
+++ com.autovend.software/src/com/autovend/software/Pay.java
@@ -1,16 +1,27 @@
 package com.autovend.software;
 
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import com.autovend.Bill;
 import com.autovend.devices.BarcodeScanner;
 import com.autovend.devices.ElectronicScale;
 
 public abstract class Pay {
-	public void printReceipt() {
-		
-	}
+	
 	ElectronicScale es = new ElectronicScale(2, 0);
 	BarcodeScanner bs = new BarcodeScanner();
 	Cart cart = new Cart(es, bs);
+	Bill bill = new Bill(1000, Currency.getInstance("USD"));
 	public abstract void pay(); 
 	
+	BigDecimal paymentDue = cart.getPrice();
+	int paymentMade = bill.getValue();
+	
+	public void printReceipt() {
+		if (paymentMade < Integer.valueOf(paymentMade)) {
+			
+		}
+	}
 	
 }
