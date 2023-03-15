30a3e78920aa17d7a236aac28a1c4fa54689f855
Nam Nguyen Vu <namnguyen.vu1@ucalgary.ca>
2023-03-15 10:22:40 -0600

    I modify pay.java

--- com.autovend.software/src/com/autovend/software/Pay.java
+++ com.autovend.software/src/com/autovend/software/Pay.java
@@ -1,10 +1,15 @@
 package com.autovend.software;
 
+import com.autovend.devices.BarcodeScanner;
+import com.autovend.devices.ElectronicScale;
+
 public abstract class Pay {
 	public void printReceipt() {
 		
 	}
-	
+	ElectronicScale es = new ElectronicScale(2, 0);
+	BarcodeScanner bs = new BarcodeScanner();
+	Cart cart = new Cart(es, bs);
 	public abstract void pay(); 
 	
 	
