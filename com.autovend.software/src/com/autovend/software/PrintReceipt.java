1b08085cbf73172f1349bd1ce3f79bbe69860e81
namnguyenvu1 <126105726+namnguyenvu1@users.noreply.github.com>
2023-03-17 17:36:11 -0600

    update feature
    update feature

--- com.autovend.software/src/com/autovend/software/PrintReceipt.java
+++ com.autovend.software/src/com/autovend/software/PrintReceipt.java
@@ -1,9 +1,23 @@
 package com.autovend.software;
 
+import java.math.BigDecimal;
+import java.util.Currency;
+
 import com.autovend.Bill;
+import com.autovend.devices.EmptyException;
+import com.autovend.devices.OverloadException;
+import com.autovend.devices.ReceiptPrinter;
 
 public class PrintReceipt {
-	public PrintReceipt(Bill bill) {
+	public PrintReceipt(Bill bill) throws OverloadException, EmptyException {
+		// To-do
+		// Print the details of the receipt
+		System.out.print(bill.getValue());
+		int amount = bill.getValue();
 		
+		// Update bills
+		bill = new Bill(amount, Currency.getInstance("USD"));
+		ReceiptPrinter receivePrinter = new ReceiptPrinter();
+		receivePrinter.print('c');
 	}
 }
