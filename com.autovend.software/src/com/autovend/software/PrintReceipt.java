6c16d5819baa9cb072838a2e15d1b9baa4bfab62
namnguyenvu1 <126105726+namnguyenvu1@users.noreply.github.com>
2023-03-17 18:08:13 -0600

    update features
    update features

--- com.autovend.software/src/com/autovend/software/PrintReceipt.java
+++ com.autovend.software/src/com/autovend/software/PrintReceipt.java
@@ -2,22 +2,52 @@ package com.autovend.software;
 
 import java.math.BigDecimal;
 import java.util.Currency;
+import java.util.HashMap;
 
 import com.autovend.Bill;
 import com.autovend.devices.EmptyException;
 import com.autovend.devices.OverloadException;
 import com.autovend.devices.ReceiptPrinter;
+import com.autovend.products.Product;
 
 public class PrintReceipt {
-	public PrintReceipt(Bill bill) throws OverloadException, EmptyException {
+	public PrintReceipt(ReceiptPrinter receiptPrinter, Cart cart) throws OverloadException, EmptyException {
 		// To-do
-		// Print the details of the receipt
-		System.out.print(bill.getValue());
-		int amount = bill.getValue();
 		
-		// Update bills
-		bill = new Bill(amount, Currency.getInstance("USD"));
-		ReceiptPrinter receivePrinter = new ReceiptPrinter();
-		receivePrinter.print('c');
+		
+		
+		// Get items, get price, then print it
+		
+		// Print the bill record
+		// Key of hashmap is product code
+		
+		HashMap<Product,Integer> map = cart.getCart();
+		
+		
+		for (Product key : map.keySet()) {
+			Integer quantity = map.get(key);	
+			BigDecimal price = key.getPrice();
+			String s = key.toString();
+			// Print char from string (receiptP);
+			for (int i = 0; i < s.length(); i++) {
+				receiptPrinter.print(s.charAt(i));
+			}
+			// Make observer react ink;
+			
+		}
+			
+		
+		// Print the receipt
+		
+		
+		// Signals to Customer I/O that the customer’s session is complete
+		
+		
+		// Thanks the Customer
+		
+		
+		// Ready for a new customer session
+		
+		
 	}
 }
