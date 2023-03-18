f1f09b2cc1f8a0d42417e329f6ff9cf0deaf3fed
aditiyv <aditi.yadav@ucalgary.ca>
2023-03-17 22:29:45 -0600

    making progress on the test class

--- com.autovend.software.test/src/com/autovend/software/test/addByScan_test.java
+++ com.autovend.software.test/src/com/autovend/software/test/addByScan_test.java
@@ -1,6 +1,8 @@
 package com.autovend.software.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.math.BigDecimal;
 
@@ -9,6 +11,7 @@ import org.junit.Test;
 
 import com.autovend.Barcode;
 import com.autovend.BarcodedUnit;
+import com.autovend.Numeral;
 import com.autovend.devices.BarcodeScanner;
 import com.autovend.devices.ElectronicScale;
 import com.autovend.devices.OverloadException;
@@ -23,16 +26,56 @@ public class addByScan_test {
     private BarcodedUnit product;
     private Barcode barcode;
     private double weightInGrams;
+    private double weightLimitInGrams;
 
     @Before
     public void setUp() {
-        scale = new ElectronicScale(0,1);
+        scale = new ElectronicScale(5,5);
         scanner = new BarcodeScanner();
         cart = new Cart(scale, scanner);
-        barcode = new Barcode();
+        Numeral[] code = {Numeral.one, Numeral.two, Numeral.three, Numeral.four};
+        Barcode barcode = new Barcode(code);
         weightInGrams = 100.0;
+        weightLimitInGrams = 90;
         product = new BarcodedUnit(barcode, weightInGrams);
+    }   
+
+    @Test
+    public void testAddByScan_AddsProductToCart() throws WeightDiscrepancyException, OverloadException {
+        assertTrue(cart.addByScan(product));
+        assertTrue(cart.getCart().containsKey(product));
+        assertEquals(1, cart.getCart().get(product).intValue());
     }
-    ////
-   
+
+    @Test
+    public void testAddByScan_UpdatesCurrentWeight() throws WeightDiscrepancyException, OverloadException {
+        assertTrue(cart.addByScan(product));
+        assertEquals(weightInGrams, cart.getCurrentWeight(), 0);
+    }
+
+    @Test
+    public void testAddByScan_UpdatesPrice() throws WeightDiscrepancyException, OverloadException {
+        assertTrue(cart.addByScan(product));
+        assertEquals(BigDecimal.ZERO, cart.getPrice());
+    }
+
+    @Test
+    public void testAddByScan_ThrowsWeightDiscrepancyException() throws OverloadException {
+        // Set scale weight to be different from product weight
+        scale.setCurrentWeight(weightInGrams + 1.0);
+
+        try {
+            cart.addByScan(product);
+            fail("Expected WeightDiscrepancyException to be thrown");
+        } catch (WeightDiscrepancyException e) {
+            // Pass
+        }
+    }
+//passed test:
+    @Test(expected = OverloadException.class)
+    public void testAddByScan_ThrowsOverloadException() throws WeightDiscrepancyException, OverloadException {
+        // Set scale weight to be greater than max weight
+        scale.setCurrentWeight(weightInGrams);
+        cart.addByScan(product);
+    }
 }
