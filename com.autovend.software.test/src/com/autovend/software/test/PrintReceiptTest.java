0e7bf3caccda19a81fccc7aa51689a8256ecb171
aditiyv <aditi.yadav@ucalgary.ca>
2023-03-19 00:19:41 -0600

    final commit

--- com.autovend.software.test/src/com/autovend/software/test/PrintReceiptTest.java
+++ com.autovend.software.test/src/com/autovend/software/test/PrintReceiptTest.java
@@ -1,6 +1,8 @@
 //BRETT LYLE 30103268
 //Nam Nguyen Vu 30154892
 //Aditi Yadav 30143652
+//Alina Mansuri 30008370
+//Adam Mogensen 30071819
 //Arun Sharma 30124252
 
 package com.autovend.software.test;
